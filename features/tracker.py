5d9e4c8e67c4e90d1dd0baf9469ec31749079e67
luigivieira <luigivieira@gmail.com>
2016-11-23 19:02:04 -0200

    Started from visualization tool

---
+++ features/tracker.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# 
+# This file is part of the Fun SDK (fsdk) project. The complete source code is
+# available at https://github.com/luigivieira/fsdk.
+#
+# Copyright (c) 2016-2017, Luiz Carlos Vieira (http://www.luiz.vieira.nom.br)
+#
+# MIT License
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import dlib
+
+__modulePath = os.path.dirname(os.path.abspath(__file__))
+"""Path of this module, required to load the face model."""
+
+__faceModel = '{}/face_model.dat'.format(__modulePath)
+"""Path and name of the face model, inside the module path."""
+
+__detector = None
+"""Object used to detect faces in images."""
+
+__predictor = None
+"""Object used to predict the positions of facial landmarks in images."""
+
+#---------------------------------------------
+def initialize():
+    """Initializes the needed objects."""
+    global __detector
+    global __predictor
+    __detector = dlib.get_frontal_face_detector()
+    __predictor = dlib.shape_predictor(__faceModel)
+
+#---------------------------------------------
+def facialLandmarks(image, numFaces = None):
+    """
+    Finds facial landmarks in a given image.
+    
+    This function uses the face detector and predictor available from the dlib
+    package (with its default face model), and is able to find the face region
+    and 68 facial landmarks. It also rebuilds the face region obtained from
+    dlib's detector based on the landmarks positions, in order to guarantee
+    that all landmarks are included in the region returned.
+    
+    Parameters
+    ------
+    image: numpy.array
+        Image data where to detect facial landmarks.
+    numFaces: int
+        Number of faces to detect/consider. The default is None, meaning that
+        all faces found should be returned. The returned number of faces might
+        be less than this number if fewer faces are detected in the image.
+    
+    Returns
+    ------
+    list of dict
+        List of dictionaries with the faces detected, including their region
+        and landmarks. The format of the return is [{'number': <int with the
+        number of the face in the image>, 'region': <tuple with the left, top,
+        right and bottom int values of the rectangular region of the face in the
+        image>, 'landmarks': <list of tuples the x and y int values of the 68
+        landmarks detected in the image>}, {...}, ...]
+    """
+    
+    #Initialize if needed
+    if __detector is None or __predictor is None:
+        initialize()
+    
+    # Detect faces in the image, getting the bounding boxes
+    # of each face
+    detectedFaces = __detector(image, 1)
+
+    # Iterate through the detected faces to find the landmark
+    # positions and recalculate their face regions (bounding boxes)
+    cnt = 0
+    faces = []
+    for number, region in enumerate(detectedFaces):
+        # Fit the shape model over the face region to 
+        # predict the positions of facial landmarks
+        faceShape = __predictor(image, region)
+
+        # Build the face dictionary and recalculate its bounding box
+        rows = image.shape[0]
+        cols = image.shape[1]
+        
+        minX = cols
+        minY = rows
+        maxX = 0
+        maxY = 0
+        landmarks = []
+        for point in faceShape.parts():
+            x = point.x
+            y = point.y
+            landmarks.append((x, y))
+            if x < minX:
+                minX = x
+            if y < minY:
+                minY = y
+            if x > maxX:
+                maxX = x
+            if y > maxY:
+                maxY = y
+
+        # Add a small margin to the recalculated region
+        margin = 10
+        minX -= margin        
+        minY -= margin
+        maxX += margin
+        maxY += margin
+        if minX < 0:
+            minX = 0
+        if minY < 0:
+            minY = 0
+        if maxX >= cols:
+            maxX = cols - 1
+        if maxY >= rows:
+            maxY = rows - 1
+                
+        # Build up the return
+        face = {
+                'number': number,
+                'region': (minX, minY, maxX, maxY),
+                'landmarks': landmarks
+               }
+        faces.append(face)
+        
+        # Check if the requested limit has been reached
+        cnt += 1
+        if numFaces is not None and cnt == numFaces:			
+            break
+
+    return faces
---
+++ viewer.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# 
+# This file is part of the Fun SDK (fsdk) project. The complete source code is
+# available at https://github.com/luigivieira/fsdk.
+#
+# Copyright (c) 2016-2017, Luiz Carlos Vieira (http://www.luiz.vieira.nom.br)
+#
+# MIT License
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import sys
+import argparse
+import numpy as np
+import cv2
+from features.tracker import facialLandmarks
+
+def main(argv):
+
+    # Parse the command line
+    args = parseCommandLine(argv)
+
+    if args.file is None:
+        video = cv2.VideoCapture(0)
+    else:
+        try:
+            video = cv2.VideoCapture(args.file)
+        except:
+            print('Error reading file {} '
+                  '(is it an image or video?)'.format(args.file))
+            sys.exit(-1)
+
+    if args.file is not None:
+        numFrames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = video.get(cv2.CAP_PROP_FPS)
+        if fps == 0:
+            delay = 0
+        else:
+            delay = int(1 / fps * 1000)
+    else:
+        delay = 1
+                
+    while video.isOpened():
+        ret, frame = video.read()
+
+        # Ignore all black windows
+        # Because of a failure in the experiment performed
+        # to capture data, all of my videos started with pure black
+        # frames :/
+        if cv2.countNonZero(frame[:,:,0]) == 0:
+            continue
+        
+        # Track only one face
+        faces = facialLandmarks(frame, 1)
+
+        # Display the frame with the face region
+        for face in faces:
+            region = face['region']
+            cv2.rectangle(frame, (region[0], region[1]), (region[2], region[3]), (255,0,0), 2)
+
+            for point in face['landmarks']:
+                cv2.circle(frame, point, 1, (0, 0, 255), 2)
+
+        cv2.imshow('Viewer', frame)
+
+        if cv2.waitKey(delay) & 0xFF == ord('q'):
+            break
+
+    video.release()
+
+#---------------------------------------------
+def parseCommandLine(argv):
+    parser = argparse.ArgumentParser(
+                        description='Display a video with the tracking of '
+                                    'facial landmarks.')
+    parser.add_argument('file', nargs='?',
+                        help='Video file to use. If not provided, the '
+                             'video capture from an existing webcam will '
+                             'be used.'
+                       )
+    
+    return parser.parse_args()
+
+#---------------------------------------------    
+def showHelp():
+    print('{} [file]'.format(__name__))
+    
+    
+# namespace verification for invoking main
+if __name__ == '__main__':
+    main(sys.argv[1:])
