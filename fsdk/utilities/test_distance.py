88612089e8aabae3445e3ee245a434fdfb451d3d
luigivieira <luigivieira@gmail.com>
2017-01-31 20:18:11 -0200

    Useful testing scripts

---
+++ fsdk/utilities/test_distance.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+#
+# This file is part of the Fun SDK (fsdk) project. The complete source code is
+# available at https://github.com/luigivieira/fsdk.
+#
+# Copyright (c) 2016-2017, Luiz Carlos Vieira (http://www.luiz.vieira.nom.br)
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import sys
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+    import sys
+    sys.path.append('../../')
+
+from fsdk.features.data import FaceData
+from fsdk.detectors.faces import FaceDetector
+
+#---------------------------------------------
+def main(argv):
+    """
+    Main entry of this script.
+
+    Parameters
+    ------
+    argv: list of str
+        Arguments received from the command line.
+    """
+
+    # Start the video playback of the FGNet Talking Face
+    video = cv2.VideoCapture(0)
+    video.set(cv2.CAP_PROP_FRAME_WIDTH,  1280);
+    video.set(cv2.CAP_PROP_FRAME_HEIGHT, 720);
+
+    if not video.isOpened():
+        print('Error opening the video input')
+        return -1
+
+    det = FaceDetector()
+
+    # Process the camera input
+    while True:
+
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        ret, face = det.detect(frame, 4)
+        if ret:
+            face.draw(frame)
+            print('Distance: {}'.format(face.distance))
+
+        cv2.imshow('Face Detector', frame)
+        key = cv2.waitKey(1)
+
+        if key == ord('q') or key == ord('Q') or key == 27:
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
+
+#---------------------------------------------
+# namespace verification for invoking main
+#---------------------------------------------
+if __name__ == '__main__':
+    main(sys.argv[1:])
