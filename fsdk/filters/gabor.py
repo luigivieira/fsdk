57f11cdf0c1ba47d5f666da26c08f8afe58eb2b9
luigivieira <luigivieira@gmail.com>
2017-01-31 20:13:41 -0200

    Allowed to receive parameters at the constructor

--- fsdk/filters/gabor.py
+++ fsdk/filters/gabor.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
-# 
+#
 # This file is part of the Fun SDK (fsdk) project. The complete source code is
 # available at https://github.com/luigivieira/fsdk.
 #
 # Copyright (c) 2016-2017, Luiz Carlos Vieira (http://www.luiz.vieira.nom.br)
 #
 # MIT License
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -35,12 +35,12 @@ class KernelParams:
     """
     A simple class to represent the parameters of a given Gabor kernel.
     """
-    
+
     #---------------------------------------------
     def __init__(self, wavelength, orientation):
         """
         Class constructor. Define the parameters of a Gabor kernel.
-        
+
         Parameters
         ----------
         wavelength: float
@@ -48,10 +48,10 @@ class KernelParams:
         orientation: float
             Orientations (in radians) of a Gabor kernel.
         """
-        
+
         self.wavelength = wavelength
         """Wavelength (in pixels) of a Gabor kernel."""
-        
+
         self.orientation = orientation
         """Orientation (in radians) of a Gabor kernel."""
 
@@ -59,7 +59,7 @@ class KernelParams:
     def __hash__(self):
         """
         Generates a hash value for this object instance.
-        
+
         Returns
         ----------
         hash: int
@@ -71,14 +71,14 @@ class KernelParams:
     def __eq__(self, other):
         """
         Verifies if this object instance is equal to another.
-        
+
         This method is the implementation of the == operator.
-        
+
         Parameters
         ----------
         other: KernelParams
             Other instance to compare with this one.
-            
+
         Returns
         ----------
         eq: bool
@@ -92,14 +92,14 @@ class KernelParams:
     def __ne__(self, other):
         """
         Verifies if this object instance is different than another.
-        
+
         This method is the implementation of the != operator.
-        
+
         Parameters
         ----------
         other: KernelParams
             Other instance to compare with this one.
-            
+
         Returns
         ----------
         neq: bool
@@ -107,39 +107,40 @@ class KernelParams:
             False otherwise.
         """
         return not(self == other)
-        
+
 #=============================================
 class GaborBank:
     """
     Represents the bank of gabor kernels.
     """
-    
+
     #---------------------------------------------
-    def __init__(self):
+    def __init__(self, w = [4, 7, 10, 13],
+                       o = [i for i in np.arange(0, np.pi, np.pi / 8)]):
         """
         Class constructor. Create a bank of Gabor kernels with a predefined set
         of wavelengths and orientations.
-        
+
         The bank is composed of one kernel for each combination of wavelength x
         orientation. For the rationale regading the choice of parameters, refer
         to the thesis text.
         """
-        
-        self._wavelengths = [4, 7, 10, 13]
+
+        self._wavelengths = w
         """
         List of wavelengths (in pixels) used to create the bank of Gabor
         kernels.
         """
-        
-        self._orientations = [i for i in np.arange(0, np.pi, np.pi / 8)]
+
+        self._orientations = o
         """
         List of orientations (in radians) used to create the bank of Gabor
         kernels.
         """
-        
+
         self._kernels = {}
         """Dictionary holding the Gabor kernels in the bank."""
-        
+
         # Create one kernel for each combination of wavelength x orientation
         for wavelength in self._wavelengths:
             for orientation in self._orientations:
@@ -148,7 +149,7 @@ class GaborBank:
                 # equation uses wavelengths - see https://en.wikipedia.org/wiki/
                 # Gabor_filter/)
                 frequency = 1 / wavelength
-                
+
                 # Create and save the kernel
                 kernel = gabor_kernel(frequency, orientation)
                 par = KernelParams(wavelength, orientation)
@@ -158,12 +159,12 @@ class GaborBank:
     def filter(self, image):
         """
         Filter the given image with the Gabor kernels in this bank.
-        
+
         Parameters
         ----------
         image: numpy.array
             Image to be filtered.
-            
+
         Returns
         -------
         responses: numpy.array
@@ -174,37 +175,37 @@ class GaborBank:
             for each kernel in the bank, since there are 4 wavelengths and 8
             orientations).
         """
-        
+
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        
+
         responses = []
         for wavelength in self._wavelengths:
-            for orientation in self._orientations:            
-            
+            for orientation in self._orientations:
+
                 # Get the kernel
                 frequency = 1 / wavelength
                 par = KernelParams(wavelength, orientation)
                 kernel = self._kernels[par]
-                
-                # Filter with both real and imaginary parts 
+
+                # Filter with both real and imaginary parts
                 real = cv2.filter2D(image, cv2.CV_32F, kernel.real)
                 imag = cv2.filter2D(image, cv2.CV_32F, kernel.imag)
-                
+
                 # The response is the magnitude of the real and imaginary
                 # responses to the filters, normalized to [-1, 1]
-                mag = cv2.magnitude(real, imag)                
+                mag = cv2.magnitude(real, imag)
                 cv2.normalize(mag, mag, -1, 1, cv2.NORM_MINMAX)
-                
+
                 responses.append(mag)
-                
+
         return np.array(responses)
-                
+
     #---------------------------------------------
     def createPlotFigure(self):
         """
-        Create a matplotlib figure with the 2D representations of the Gabor 
+        Create a matplotlib figure with the 2D representations of the Gabor
         kernels in this bank.
-       
+
         Returns
         -------
         fig: matplotlib.figure
@@ -215,20 +216,20 @@ class GaborBank:
         numO = len(self._orientations)
 
         fig, axarr = plt.subplots(numW, numO)
-        
+
         # This is the dimension of each kernel image representation
         rows = 64
         cols = 64
-        
+
         for w, wavelength in enumerate(self._wavelengths):
             for o, orientation in enumerate(self._orientations):
                 par = KernelParams(wavelength, orientation)
                 k = np.array(self._kernels[par].real)
                 cv2.normalize(k, k, -1, 1, cv2.NORM_MINMAX)
-                
+
                 img = np.zeros((rows, cols), np.float)
                 img[:,:] = np.mean(k)
-                
+
                 y = int(img.shape[0] / 2 - k.shape[0] / 2)
                 x = int(img.shape[1] / 2 - k.shape[1] / 2)
 
@@ -237,7 +238,7 @@ class GaborBank:
                 im = axarr[w, o].imshow(img, cmap='hot', vmin=-1, vmax=1)
                 axarr[w, o].set_xticks([])
                 axarr[w, o].set_yticks([])
-                
+
                 if w == numW - 1:
                     degrees = orientation * 180 / np.pi
                     if degrees - int(degrees) == 0:
@@ -245,10 +246,10 @@ class GaborBank:
                     else:
                         label = '{:2.1f}$\degree$'.format(degrees)
                     axarr[w, o].set_xlabel(label)
-                
+
                 if o == 0:
                     axarr[w, o].set_ylabel('{:2d}'.format(wavelength))
-        
+
         fig.text(0.5, 0.04, 'Orientations (in degrees)',
                             ha='center', fontsize=15)
         fig.text(0.09, 0.5, 'Wavelenghts (in pixels)', va='center',
@@ -256,26 +257,26 @@ class GaborBank:
 
         fig.suptitle('Bank of Gabor Kernels (real parts only)', fontsize=35)
         fig.set_facecolor('white')
-        
+
         # Add the source image and a colorbar
         fig.subplots_adjust(right=0.9)
-        
+
         axis = fig.add_axes([0.92, 0.15, 0.02, 0.7])
         fig.colorbar(im, cax=axis)
-        
+
         return fig
-        
+
     #---------------------------------------------
     def createTestPlotFigure(self, image):
         """
         Create a matplotlib figure with the responses of the bank to the given
         image.
-       
+
         Parameters
         ----------
         image: numpy.array
             Image to be filtered with each Gabor kernel in this bank.
-            
+
         Returns
         -------
         fig: matplotlib.figure
@@ -285,23 +286,23 @@ class GaborBank:
         # (for displaying)
         source = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        
+
         # Create a new figure with a subplot for each kernel
         numW = len(self._wavelengths)
         numO = len(self._orientations)
 
         fig, axarr = plt.subplots(numW, numO)
-        
+
         for w, wavelength in enumerate(self._wavelengths):
             for o, orientation in enumerate(self._orientations):
                 par = KernelParams(wavelength, orientation)
                 kernel = self._kernels[par]
-                
+
                 real = cv2.filter2D(image, cv2.CV_32F, kernel.real)
                 imag = cv2.filter2D(image, cv2.CV_32F, kernel.imag)
                 response = cv2.magnitude(real, imag)
                 cv2.normalize(response, response, -1, 1, cv2.NORM_MINMAX)
-                    
+
                 im = axarr[w, o].imshow(response, cmap='hot', vmin=-1, vmax=1)
                 axarr[w, o].set_xticks([])
                 axarr[w, o].set_yticks([])
@@ -314,7 +315,7 @@ class GaborBank:
                     axarr[w, o].set_xlabel(label)
                 if o == 0:
                     axarr[w, o].set_ylabel('{:2d}'.format(wavelength))
-        
+
         # Add the titles
         fig.text(0.56, 0.06, 'Orientations (in degrees)',
                             ha='center', fontsize=15)
@@ -323,17 +324,17 @@ class GaborBank:
 
         fig.suptitle('Responses of the bank of Gabor kernels', fontsize=35)
         fig.set_facecolor('white')
-        
+
         # Add the source image and a colorbar
         fig.subplots_adjust(left=0.22, right=0.9)
-        
+
         axis = fig.add_axes([0.0, 0.7, 0.2, 0.2])
         axis.imshow(source, vmin=0, vmax=255)
         axis.set_xticks([])
         axis.set_xlabel('Original Image', fontsize=15)
         axis.set_yticks([])
-        
+
         axis = fig.add_axes([0.92, 0.15, 0.02, 0.7])
         fig.colorbar(im, cax=axis)
-        
+
         return fig
